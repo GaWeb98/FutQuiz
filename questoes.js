a8f8134d1ffcc4f098b62619a2ad1e6f1d9575b1
GaWeb98 <gabrielalves1062014@gmail.com>
2023-01-27 01:53:01 -0300

    Atualização Quiz

---
+++ questoes.js
@@ -0,0 +1,185 @@
+Questoes = [
+    {
+        Questao: 'Qual o atual campeão brasileiro? (2023)',
+        Opcoes: [
+            'Cruzeiro',
+            'Flamengo',
+            'Corinthians',
+            'Palmeiras'
+        ],
+        Correta: 3
+    },
+
+    {
+        Questao: 'Qual o primeiro campeão da copa do Brasil?',
+        Opcoes: [
+            'Grêmio',
+            'Cruzeiro',
+            'Corinthians',
+            'Botafogo'
+        ],
+        Correta: 0
+    },
+
+    {
+        Questao: 'Quem são os 3 maiores artilheiros do brasileirão na era dos pontos corridos?',
+        Opcoes: [
+            'Fred, Guerrero e Rafael Moura',
+            'Gabigol, Diego Souza e Ronaldo',
+            'Fred, Diego Souza e Wellington Paulista',
+            'Neymar, Ronaldinho e Luis Fabiano'
+        ],
+        Correta: 2
+    },
+
+    {
+        Questao: 'Qual destes é o maior campeão da Copa do Brasil?',
+        Opcoes: [
+            'Grêmio',
+            'Internacional',
+            'Corinthians',
+            'Cruzeiro'
+        ],
+        Correta: 3
+    },
+
+    {
+        Questao: 'Quem fez mais gols em uma única edição do campeonato brasileiro?',
+        Opcoes: [
+            'Washngton',
+            'Edmundo',
+            'Fred',
+            'Gabigol',
+            'Dimba',
+            'Neymar',
+            'Edílson Capetinha',
+            'Romário'
+        ],
+        Correta: 0
+    },
+
+    {
+        Questao: 'Quem é o maior campeão do campeonato brasileiro, segundo a CBF?',
+        Opcoes: [
+            'Corinthians',
+            'Internacional',
+            'Santos',
+            'Palmeiras'
+        ],
+        Correta: 3
+    },
+
+    {
+        Questao: 'Qual dessas é a única equipe a vencer o brasileirão de forma invicta?',
+        Opcoes: [
+            'Atlético Mineiro',
+            'Internacional',
+            'São Paulo',
+            'Corinthians'
+        ],
+        Correta: 1
+    },
+
+    {
+        Questao: 'Qual desses grandes clubes nunca foi campeão da Copa do Brasil?',
+        Opcoes: [
+            'Santos',
+            'São Paulo',
+            'Fluminense',
+            'Internacional'
+        ],
+        Correta: 1
+    },
+
+    {
+        Questao: 'Entres essas equipes, quem tem mais rebaixamentos na Série A do brasileirão?',
+        Opcoes: [
+            'Chapecoense',
+            'Grêmio',
+            'Vasco',
+            'Fluminense'
+        ],
+        Correta: 2
+    },
+
+    {
+        Questao: 'Qual destas equipes não tem bicampeonato do brasileirão?',
+        Opcoes: [
+            'Fluminense',
+            'Atlético Mineiro',
+            'Botafogo',
+            'Athletico-PR'
+        ],
+        Correta: 3
+    },
+    {
+        Questao: 'Em que ano ocorreu a primeira edição do Brasileirão no modelo pontos corridos?',
+        Opcoes: [
+            '2000',
+            '2001',
+            '2002',
+            '2003'
+        ],
+        Correta: 3
+    },
+    {
+        Questao: 'Quem foi o primeiro campeão do Brasileirão na era dos pontos corridos?',
+        Opcoes: [
+            'Santos',
+            'Cruzeiro',
+            'Bahia',
+            'Athletico-PR'
+        ],
+        Correta: 1
+    },
+    {
+        Questao: 'Qual time fez a pior campanha da historia dos pontos corridos?',
+        Opcoes: [
+            'America de Natal',
+            'Chapecoense',
+            'Ipatinga',
+            'Santa Cruz'
+        ],
+        Correta: 1
+    },
+    {
+        Questao: 'Qual é o time com maior pontuação acumulada na era dos pontos corridos?',
+        Opcoes: [
+            'Flamengo',
+            'Corinthians',
+            'São Paulo',
+            'Palmeiras'
+        ],
+        Correta: 2
+    },
+    {
+        Questao: 'Em que ano aconteceu a primeira Edição da Copa do Brasil?',
+        Opcoes: [
+            '1980',
+            '1999',
+            '1989',
+            '1979'
+        ],
+        Correta: 2
+    },
+    {
+        Questao: 'Quem são os dois maiores artilheiros da historia Copa do Brasil?',
+        Opcoes: [
+            'Fred e Romário',
+            'Oséas e Paulo Nunes',
+            'Viola e Dodô',
+            'Luis Fabiano e Gabigol'
+        ],
+        Correta: 0
+    },
+    {
+        Questao: 'Qual é o primeiro campeã da copa do Brasil?',
+        Opcoes: [
+            'Cruzeiro',
+            'Paysandu',
+            'Corinthians',
+            'Grêmio'
+        ],
+        Correta: 3
+    },
+]
---
+++ script.js
@@ -0,0 +1,113 @@
+document.querySelector('.apresentacao button').addEventListener('click', () => {
+    document.querySelector('.apresentacao').style.display = 'none';
+    mostrarQuestoes();
+});
+let questaoAtual = 0;
+let questaoCorreta = 0;
+
+function mostrarQuestoes() {
+    if(Questoes[questaoAtual]) {
+
+    let pct = Math.floor((questaoAtual / Questoes.length) * 100);
+
+    document.querySelector('.QuestoesArea').style.display = 'flex';
+    document.querySelector('.Progresso').style.display = 'flex';
+    document.querySelector('.QuantidadeQuestoes').style.display = 'flex';
+    document.querySelector('.QuantidadeQuestoes').innerHTML = `Questão ${parseInt(questaoAtual)+1} de ${Questoes.length}`;
+
+    document.querySelector('.Questao span').innerHTML = Questoes[questaoAtual].Questao;
+    document.querySelector('.Progresso').style.width = `${pct}%`;
+
+
+    let opcoesHTML = '';
+    for(let i in Questoes[questaoAtual].Opcoes) {
+        opcoesHTML += `<div class="Opcao"  data-op="${i}"><span>${parseInt(i)+1}</span>${Questoes[questaoAtual].Opcoes[i]}</div>`;
+    }
+    document.querySelector('.Opcoes').innerHTML = opcoesHTML;
+    document.querySelector('.Opcoes').addEventListener('click', OpcaoSelected);
+    } else {
+    fim();
+    };
+};
+//mecânica de selecionar e desselecionar elementos
+function OpcaoSelected(e) {
+    if(e.target.dataset.selected) {
+        e.target.removeAttribute('data-selected', 'selected');
+        
+    } else {
+        limpaSelected();
+        e.target.setAttribute('data-selected', 'selected');
+    }
+
+};
+function limpaSelected () {
+   let opcoes = document.querySelectorAll('.Opcao');
+   let opcoesArray = Array.from(opcoes);
+
+   opcoesArray.map(el => {
+    el.removeAttribute('data-selected', 'selected');
+   });
+};
+
+//evento de click para a próxima questão
+document.querySelector('.proximo').addEventListener('click', proximo);
+function proximo() {
+    let opcoes = document.querySelectorAll('.Opcao');
+    let opcoesArray = Array.from(opcoes);
+
+    let opcoesArrayIndex = opcoesArray.findIndex(el => {
+        return (el.dataset.selected) ? true : false
+    });
+
+    let opcoesArrayRes = opcoesArrayIndex;
+    console.log(opcoesArrayRes);
+
+    if(opcoesArrayRes > -1) {
+        try {
+            opcoesArray.map(el => {
+                let SelectedOp = parseInt(el.getAttribute('data-op'));
+                if(el.dataset.selected && Questoes[questaoAtual].Correta === SelectedOp) {
+                    questaoCorreta++;
+                };
+            });
+            //adiciona uma cor verde na alternativa correta e vermelho nas demais
+            opcoesArray.map(el => {
+                let crt = Questoes[questaoAtual].Correta;
+                let SelectedOp = parseInt(el.getAttribute('data-op'));
+                if(SelectedOp === crt) {
+                    el.classList.add('QuestaoCorreta');
+                    el.classList.add('Correta');
+                } else {
+                    el.classList.add('QuestaoErrada');
+                }
+            });
+
+            questaoAtual++;
+            let Timer = setInterval(function() {
+                mostrarQuestoes();
+                clearInterval(Timer);
+            }, 1000);
+        }
+        catch (error) {
+            console.log(error);
+        };
+    };
+};
+
+function fim() {
+    let pontos = Math.floor((questaoCorreta / Questoes.length) * 100);
+
+    document.querySelector('.QuestoesArea').style.display = 'none';
+    document.querySelector('.PlacarPontos').style.display = 'flex';
+    document.querySelector('.Progresso').style.width = '100%';
+
+    document.querySelector('.PlacarPontos h2').innerHTML = `Você acertou ${pontos}%`;
+    document.querySelector('.PlacarPontos span').innerHTML = `Você respondeu ${questaoAtual} questões e acertou ${questaoCorreta}`;
+}
+
+document.querySelector('.PlacarPontos button').addEventListener('click', () => {
+    questaoAtual = 0;
+    questaoCorreta = 0;
+    mostrarQuestoes();
+    document.querySelector('.PlacarPontos').style.display = 'none'
+});
